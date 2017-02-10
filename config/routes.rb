efb15b257722f9a595e60ce56dadae5b0cee3e41
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-02-10 09:55:25 +0900

    Configured custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
 
-  get 'pages/about'
+  get "about", to: "pages#about"
 
   get 'pages/contact'
 
   resources :blogs
 
-
+  root to: "pages#home"
 
 
 
